a24587a0c0eb506cc275c18217d80522fe4d41d7
lucasptree <162895321+lucasptree@users.noreply.github.com>
2024-09-05 19:58:28 -0300

    Update dados.js
    Update:
    - organização dos dados

--- dados.js
+++ dados.js
@@ -1,14 +1,19 @@
-let dados = [
-    {
-    cargo: "Guerreiro",
-    poder: 5,
-},
-    {
-    cargo: "Maga",
-    poder: 2,
-},
-    {
-    cargo: "Patrulheiro",
-    poder: 3,
-}
-]
+// Lista de heróis
+let dadosHerois = [
+    { cargo: "Guerreiro", poder: 5 },
+    { cargo: "Maga", poder: 2 },
+    { cargo: "Patrulheiro", poder: 3 }
+];
+
+// Lista de itens com nomes de armas de fantasia e poderes de 1 a 9
+let dadosItens = [
+    { nome: "Espada Flamejante", poder: 7 },
+    { nome: "Arco de Gelo", poder: 4 },
+    { nome: "Machado de Pedra", poder: 5 },
+    { nome: "Cajado Místico", poder: 3 },
+    { nome: "Lâmina da Tempestade", poder: 8 },
+    { nome: "Escudo de Luz", poder: 2 },
+    { nome: "Clava do Trovão", poder: 6 },
+    { nome: "Daga Sombria", poder: 1 },
+    { nome: "Armadura de Ferro", poder: 9 }
+];
